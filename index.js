1e01d72d5aa07725547ea295366f16ad3040fb45
Kevin Lydon <klydon@broadinstitute.org>
2021-05-17 10:49:17 -0400

    Added an example test config for setting up a test in carrot

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
 const yargs = require("yargs");
 const fs = require('fs');
 
-function make_data(m) {
+function make_data(a, b) {
     let x = [];
     let y = [];
-    for(let i = 0.0; i <= 20.0; i++) {
-        x.push(i/10.0);
-        y.push(Math.pow(m, i/10.0));
+    for(let i = 0.0; i < 2.1; i+=0.1) {
+        x.push(i);
+        y.push(a * Math.pow(b, i));
     }
     return [x, y];
 }
@@ -16,7 +16,7 @@ function make_data(m) {
 function print_data_as_csv(x, y) {
     let csv_string = "";
     for(let i = 0; i < x.length; i++) {
-        csv_string += `${x[i]},${y[i]}\n`;
+        csv_string += `${x[i].toFixed(1)},${y[i]}\n`;
     }
     return csv_string;
 }
@@ -27,8 +27,8 @@ function load_file(filename) {
     let y = [];
     for(let i = 0; i < data.length; i++) {
         const split_row = data[i].split(',');
-        x.push(split_row[0]);
-        y.push(split_row[1]);
+        x.push(parseFloat(split_row[0]));
+        y.push(parseFloat(split_row[1]));
     }
     return [x, y];
 }
@@ -36,7 +36,7 @@ function load_file(filename) {
 function compare_to_default(x, y) {
     let diff_y = [];
     for(let i = 0; i < x.length; i++) {
-        diff_y.push(Math.abs(y[i] - Math.pow(1.5, x[i]/10.0)));
+        diff_y.push(Math.abs(y[i] - Math.pow(2.0, x[i])));
     }
     return [x, diff_y];
 }
@@ -44,17 +44,22 @@ function compare_to_default(x, y) {
 yargs
     .scriptName("example-test")
     .usage('$0 <cmd> [args]')
-    .command('make-data [m]', "prints csv-format string for m^x for values of x from 0 to 2 in increments of 0.1", (yargs) => {
-        yargs.positional('m', {
+    .command('make-data [a] [b]', "prints csv-format string for ab^x for values of x from 0 to 2 in increments of 0.1", (yargs) => {
+        yargs.positional('a', {
+            type: 'number',
+            default: '1',
+            describe: 'the a in ab^x'
+        }),
+        yargs.positional('b', {
             type: 'number',
             default: '1.5',
-            describe: 'the base number for the exponential function that will generate the data'
+            describe: 'the b in ab^x'
         })
     }, function (argv) {
-        const data = make_data(argv.m);
+        const data = make_data(argv.a, argv.b);
         console.log(print_data_as_csv(data[0], data[1]));
     })
-    .command('compare [file]', "prints a csv-format string for differences at each x between values in file and values for 1.5^x", (yargs) => {
+    .command('compare [file]', "prints a csv-format string for differences at each x between values in file and values for 2^x", (yargs) => {
         yargs.positional('file', {
             type: 'string',
             describe: 'the file to read the data to compare from'
